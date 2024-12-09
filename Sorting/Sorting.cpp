e4c1bac861506f28537f5af009503afec2608a87
Chusikls <Chusikl@yandex.ru>
2024-12-09 11:32:23 +0300

    Добавьте файлы проекта.

---
+++ Sorting/Sorting.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+    float number[15];
+    for (int n = 0; n < 15; n++) {
+        std::cout << "Enter float numbers: \n";
+        std::cin >> number[n];
+    }
+    for (int n = 0; n < 15; n++) {
+        for (int i = n + 1; i < 15; i++) {
+            if (number[i] > number[n]) std::swap(number[i], number[n]);
+        }
+    }
+    for (int n = 0; n < 15; n++) {
+        std::cout << number[n] << ' ';
+    }
+}
+
+
